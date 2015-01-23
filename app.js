6c534629233abeffd3f29c6c982c6c8eff6945fa
Glenn Davis <glenn@gdcreative.com>
2015-01-23 16:34:41 -0500

    Template

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var swig = require('swig');
+swig.setDefaults({ cache: false });
 
 var app = express();
 app.engine('html', swig.renderFile);
@@ -9,12 +10,17 @@ app.set('views','./views');
 
 app.use(morgan('dev'));
 
-app.get('/', function (req, res) {
-  res.send('whoop!');
-});
+// app.get('/', function (req, res) {
+//   res.send('whoop!');
+// });
+
+// app.get('/tweets', function (req, res) {
+// 	res.send('Tweet!');
+// });
 
 app.get('/tweets', function (req, res) {
-	res.send('Tweet!');
+	var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+	res.render( 'index', {title: 'Hall of Fame', people: people} );
 });
 
 var server = app.listen(3000, function () {
